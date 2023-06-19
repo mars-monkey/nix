6b207be04befbe8522c8136e6ac77620b901f0d7
starship-boi <91227993+starship-boi@users.noreply.github.com>
2023-06-19 18:21:07 +0000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -19,60 +19,62 @@
   };
 
   networking = {
-    hostName = "nixos";
-
-    # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-    # proxy = { default = "http://user:password@proxy:port/"; noProxy = "127.0.0.1,localhost,internal.domain"; };
-
+    hostName = "nixos-test";
     networkmanager.enable = true;
+    
+    # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    # proxy = { default = "http://user:password@proxy:port/"; noProxy = "127.0.0.1,localhost,internal.domain"; };
   };
 
-  # Set your time zone.
   time.timeZone = "Africa/Douala";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
-  
   services = {
     xserver = {
       enable = true;
       layout = "us";
       xkbVariant = "";
-      displayManager.gdm.enable = true;
+      
+      displayManager = {
+        gdm.enable = true;
+        
+        autoLogin = {
+          enable = false;
+          user = "noahh";
+        };
+      };
+      
       desktopManager.gnome.enable = true;
+      
+      # libinput.enable = true; # Enable touchpad support (enabled default in most desktopManager).
     };
 
+    printing.enable = true;
+
+    sound.enable = true;
+    hardware.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+
+      # If you want to use JACK applications, uncomment this
+      # jack.enable = true;
+
+      # use the example session manager (no others are packaged yet so this is enabled by default,
+      # no need to redefine it in your config for now)
+      # media-session.enable = true;
+    };
+    
+    flatpak.enable = true;
+    
     gnome.core-utilities.enable = false;
   };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  services.flatpak.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # Don't forget to set a password with ‘passwd’.
   users.users.nix = {
     isNormalUser = true;
     description = "nix";
@@ -87,24 +89,21 @@
     ];
   };
 
-  # Enable automatic login for the user.
-  services.xserver.displayManager.autoLogin.enable = false;
-  services.xserver.displayManager.autoLogin.user = "noahh";
-
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  pkgs.flatpak
-  wget
+    flatpak
+    wget
   ];
 
   # enable gtk themes for qt apps
-  qt5.enable = true;
-  qt5.platformTheme = "gtk2";
-  qt5.style = "gtk2";
+  qt5 = {
+    enable = true;
+    platformTheme = "gtk2";
+    style = "gtk2";
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -133,7 +132,8 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.11"; # Did you read the comment?
 
-  # auto upgrades
-  system.autoUpgrade.enable = true;
-  system.autoUpgrade.allowReboot = true;
+  system.autoUpgrade = {
+    enable = true;
+    allowReboot = true;
+  };
 }

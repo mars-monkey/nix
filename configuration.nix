de19e8a7d06a88632b0f5f11d49e08fce3564a42
starship-boi <91227993+starship-boi@users.noreply.github.com>
2023-06-19 17:50:18 +0000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -10,10 +10,14 @@
       ./hardware-configuration.nix
     ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # Bootloader
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot/efi";
+    };
+  };
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.

d0f3604d48d55d93fedb50358026903bd4a1ff48
starship-boi <91227993+starship-boi@users.noreply.github.com>
2023-06-19 17:59:01 +0000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,12 +1,11 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+# Help is available in the configuration.nix(5) man page and
+# in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
     ];
 
@@ -20,13 +19,13 @@
   };
 
   networking = {
-    hostName = "nixos"; # Define your hostname.
+    hostName = "nixos";
     # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
     # Configure network proxy if necessary
     # proxy = { default = "http://user:password@proxy:port/"; noProxy = "127.0.0.1,localhost,internal.domain"; };
 
-    networkmanager.enable = true; # Enable networking
+    networkmanager.enable = true;
   };
 
   # Set your time zone.
@@ -35,8 +34,15 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  
+  services = {
+    xserver = {
+      # Enable the X11 windowing system.
+      enable = true;
+    };
+  };
+
+.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;

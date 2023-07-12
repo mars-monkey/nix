cb8a247a7ef2294a74678fd72669024d18a0a0b7
mars-monkey <91227993+mars-monkey@users.noreply.github.com>
2023-07-12 04:50:34 +0100

    Local changes autocommit

---
+++ home/gp.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Script to push local git repo changes to a GitHub repo
+# Must be called with path to git repo as argument
+
+original_working_directory=$(pwd)
+
+cd $1
+
+git commit -a -m "Local changes autocommit"
+
+git push
+
+cd $original_working_directory

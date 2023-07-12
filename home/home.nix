2ecdd112f279745acfe202f900d779c5ee5f59b0
mars-monkey <91227993+mars-monkey@users.noreply.github.com>
2023-07-12 05:14:50 +0100

    Local changes autocommit

--- home/home.nix
+++ home/home.nix
@@ -160,11 +160,7 @@
         "pop-shell@system76.com"
         "AlphabeticalAppGrid@stuarthayhurst"
       ];
-      disabled-extensions = [
-       /* "native-window-placement@gnome-shell-extensions.gcampax.github.com"
-        "window-list@gnome-shell-extensions.gcampax.github.com"
-        "windowsNavigator@gnome-shell-extensions.gcampax.github.com"*/
-      ];
+      disabled-extensions = [];
     };
   };
 }

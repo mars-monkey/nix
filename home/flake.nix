1ad75d616c0345b227c245854fd025fe86371cbc
mars-monkey <91227993+mars-monkey@users.noreply.github.com>
2023-08-24 12:54:01 +0100

    Local changes autocommit

--- home/flake.nix
+++ home/flake.nix
@@ -3,9 +3,10 @@
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
--- system/flake.nix
+++ system/flake.nix
@@ -1,7 +1,7 @@
 {
   description = "mars-monkey's NixOS system configuration flake";
   
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-23.05;
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   
   outputs = { self, nixpkgs, ... }:
     let

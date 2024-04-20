50f9ec6253880b426fd77e421888d8a4d5dd52bb
mars-monkey <91227993+mars-monkey@users.noreply.github.com>
2024-04-20 17:51:59 +0100

    Local changes autocommit

--- system/configuration.nix
+++ system/configuration.nix
@@ -131,6 +131,21 @@
     printing.enable = true;
     flatpak.enable = true;
     gvfs.enable = true;
+
+    auto-cpufreq = {
+      enable = true;
+      settings = {
+        battery = {
+          governor = "powersave";
+          turbo = "never";
+        };
+
+        charger = {
+          governor = "performance";
+          turbo = "auto";
+        };
+      };
+    };
     
     gnome = {
       gnome-keyring.enable = true;

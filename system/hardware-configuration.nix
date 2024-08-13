1341f3750f280968a26a028000918589b2f378ad
mars-monkey <91227993+mars-monkey@users.noreply.github.com>
2024-08-13 12:58:00 -0700

    Local changes autocommit

--- system/hardware-configuration.nix
+++ system/hardware-configuration.nix
@@ -59,5 +59,5 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableAllFirmware;
 }
